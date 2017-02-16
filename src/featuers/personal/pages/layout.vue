bd3ca76c0eca3189e3bcd515b6eb33187dfe8cda
pans0911 <huzhengpan@ahqianmo.com>
2017-02-16 10:30:14 +0800

    地址列表

---
+++ src/featuers/personal/pages/layout.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  //一个检测
+</script>
--- src/featuers/personal/router.js
+++ src/featuers/personal/router.js
@@ -1,6 +1,19 @@
+const R_PERSONAL_LAYOUT = r => require.ensure([], () => r(require('./pages/layout')), '/record')
 const R_PERSONAL = r => require.ensure([], () => r(require('./pages/index')), '/personal/')
+const R_ADDRESS = r => require.ensure([], () => r(require('./pages/address/list')), '/personal/')
 
 export default  {
-  path: '/personal', 
-  component:R_PERSONAL
+  path: '/personal',
+  redirect:'/personal/',
+  component:R_PERSONAL_LAYOUT,
+  children: [
+    {
+      path: '/',
+      component: R_PERSONAL
+    },
+    {
+      path: 'address',
+      component: R_ADDRESS
+    }
+  ]
 }
