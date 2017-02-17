cee43d8996d1f1a8905ef817e28e29a2aa5f561e
pans0911 <huzhengpan@ahqianmo.com>
2017-02-17 10:07:01 +0800

    地址列表、新增地址、地址编辑UI

--- src/featuers/personal/router.js
+++ src/featuers/personal/router.js
@@ -1,6 +1,8 @@
 const R_PERSONAL_LAYOUT = r => require.ensure([], () => r(require('./pages/layout')), '/record')
 const R_PERSONAL = r => require.ensure([], () => r(require('./pages/index')), '/personal/')
 const R_ADDRESS = r => require.ensure([], () => r(require('./pages/address/list')), '/personal/')
+const R_ADDRESS_ADD = r => require.ensure([], () => r(require('./pages/address/add')), '/personal/')
+const R_ADDRESS_EDIT = r => require.ensure([], () => r(require('./pages/address/edit')), '/personal/')
 
 export default  {
   path: '/personal',
@@ -12,8 +14,16 @@ export default  {
       component: R_PERSONAL
     },
     {
-      path: 'address',
+      path: 'address_list',
       component: R_ADDRESS
+    },
+    {
+      path: 'address_add',
+      component: R_ADDRESS_ADD
+    },
+    {
+      path: 'address_edit',
+      component: R_ADDRESS_EDIT
     }
   ]
 }
