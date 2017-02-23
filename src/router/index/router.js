e2d9276832f5f1e777057121a6d0f9206fccf45f
Hope <hopefullstack@gmail.com>
2017-02-23 09:09:25 +0800

    提交

--- src/router/index/router.js
+++ src/router/index/router.js
@@ -8,6 +8,16 @@ export default [
   },
   {
     //将OPENID注入session
+    path: '/home/:id/:redirectUrl/', redirect: to => {
+      store.dispatch({
+        type: 'setSession',
+        amount: to.params.id
+      })
+      return `/${to.params.redirectUrl}/`
+    }
+  },
+  {
+    //将OPENID注入session
     path: '/home/:id/', redirect: to => {
       store.dispatch({
         type: 'setSession',
