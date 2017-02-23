344eb34e8f2c823aefe4951956b897ee7bcecdd2
pans0911 <huzhengpan@ahqianmo.com>
2017-02-23 15:40:49 +0800

    proxy

--- src/config/http.js
+++ src/config/http.js
@@ -5,8 +5,7 @@ import store from 'src/store'
 
 axios.defaults.timeout = 5000;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
-axios.defaults.baseURL = 'http://192.168.5.14:8081';
-// axios.defaults.baseURL = 'http://192.168.1.129:8383';
+axios.defaults.baseURL = '';
 
 //POST传参序列化
 axios.interceptors.request.use((config) => {
