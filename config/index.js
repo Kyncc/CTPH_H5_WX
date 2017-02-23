74875e8681a6875d002ec2f91e9114b1bcafa2df
pans0911 <huzhengpan@ahqianmo.com>
2017-02-23 16:03:16 +0800

    noa

--- config/index.js
+++ config/index.js
@@ -29,7 +29,8 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': http_url
+      '/api': http_url,
+      '/noa': http_url,
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
